0ab871ec1087dfcd667dc70688bba937ab2cb2d6
Joako <151996187+joakog94@users.noreply.github.com>
2025-04-18 12:03:39 -0300

    Update data.js

--- src/data/data.js
+++ src/data/data.js
@@ -48,14 +48,14 @@ const data = {
       title: 'E-commerce Website',
       description:
         'Designed and developed a feature-rich e-commerce platform full responsive, using HTML and CSS3',
-      link: 'https://proyecto1-joachim-gomez.netlify.app/',
+      link: 'https://proyecto-1-joachim-gomez.netlify.app/',
       preview: '/assets/Proyecto-1.png'
     },
     {
       title: 'E-commerce Website functional',
       description:
         'Designed and developed a feature-rich e-commerce platform full responsive with filters, using HTML, CSS3 and JavaScript',
-      link: 'https://proyecto2-joachim-gomez.netlify.app/',
+      link: 'https://proyecto-2-joachim-gomez.netlify.app/',
       preview: '/assets/Proyecto-2.png'
     },
     {
